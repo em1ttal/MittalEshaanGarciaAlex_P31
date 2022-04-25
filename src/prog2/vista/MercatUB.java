c2c6f971173f65a1c39fca63bc44dd01fdcfcfc8
Eshaan Mittal <eshaan.mittal1@gmail.com>
2022-04-25 16:52:32 +0200

    added try catch

--- src/prog2/vista/MercatUB.java
+++ src/prog2/vista/MercatUB.java
@@ -22,6 +22,9 @@ public class MercatUB {
         gestioMenu(sc);
     }
 
+    /**
+     * Main menu options
+     */
     private static enum MENU_OPTIONS {
         M_Opcion_1_GestioArticles,
         M_Opcion_2_GestioClient,
@@ -31,18 +34,27 @@ public class MercatUB {
         M_Opcion_6_Sortir
     }
 
+    /**
+     * Article menu options
+     */
     private static enum ARTICLE_OPTIONS {
         M_Opcion_1_AfegirArticle,
         M_Opcion_2_VisualitzarArticles,
         M_Opcion_3_Sortir
     }
 
+    /**
+     * Client menu options
+     */
     private static enum CLIENT_OPTIONS {
         M_Opcion_1_AfegirClient,
         M_Opcion_2_VisualitzarClients,
         M_Opcion_3_Sortir
     }
 
+    /**
+     * Order menu options
+     */
     private static enum ORDER_OPTIONS {
         M_Opcion_1_AfegirComanda,
         M_Opcion_2_EsborrarComanda,
@@ -51,6 +63,9 @@ public class MercatUB {
         M_Opcion_5_Sortir
     }
 
+    /**
+     * Main menu text
+     */
     private static final String[] mainMenu = {
             "Gestio articles", //Option 1
             "Gestio clients",  //Option 2
@@ -60,18 +75,27 @@ public class MercatUB {
             "Sortir"           //Option 6
     };
 
+    /**
+     * Article menu text
+     */
     private static final String[] articleMenu = {
             "Afegir articles",      //Option 1
             "Visualitzar articles", //Option 2
             "Sortir"                //Option 3
     };
 
+    /**
+     * Client menu text
+     */
     private static final String[] clientMenu = {
             "Afegir client",      //Option 1
             "Visualitzar client", //Option 2
             "Sortir"              //Option 3
     };
 
+    /**
+     * Order menu text
+     */
     private static final String[] orderMenu = {
             "Afegir comanda",               //Option 1
             "Esborrar comanda",             //Option 2
@@ -80,6 +104,12 @@ public class MercatUB {
             "Sortir"                        //Option 5
     };
 
+    /**
+     * Asks for article info
+     * Passes info to Adaptador.java
+     * @param sc
+     * @throws MercatException
+     */
     private void addArticle(Scanner sc) throws MercatException {
         System.out.print("Identificador del articulo: "); String identifier = sc.nextLine();
         System.out.print("Nombre del articulo: "); String name = sc.nextLine();
@@ -89,6 +119,12 @@ public class MercatUB {
         adaptador.afegirArticle(identifier, name, price, time, priority);
     }
 
+    /**
+     * Asks for Client info
+     * Passes info to Adaptador.java
+     * @param sc
+     * @throws MercatException
+     */
     private void addClient(Scanner sc) throws MercatException {
         System.out.print("Email del cliente: "); String email = sc.nextLine();
         System.out.print("Nombre del cliente: "); String name = sc.nextLine();
@@ -97,6 +133,12 @@ public class MercatUB {
         adaptador.afegirClient(email, name, address, premium);
     }
 
+    /**
+     * Asks for order info
+     * Passes info to Adaptador.java
+     * @param sc
+     * @throws MercatException
+     */
     private void addOrder(Scanner sc) throws MercatException {
         System.out.print("Posicion del articulo: "); int posA = sc.nextInt(); sc.nextLine();
         System.out.print("Posicion del cliente: "); int posc = sc.nextInt(); sc.nextLine();
@@ -105,6 +147,12 @@ public class MercatUB {
         adaptador.afegirComanda(posA, posc, quantity, urgent);
     }
 
+    /**
+     * Asks for position order to delete
+     * Passes position to Adaptador.java
+     * @param sc
+     * @throws MercatException
+     */
     private void delOrder(Scanner sc) throws MercatException {
         System.out.print("Posicion de pedido que quieres borrar: ");
         int pos = sc.nextInt(); sc.nextLine();
@@ -112,6 +160,11 @@ public class MercatUB {
         System.out.println("Pedido borrado");
     }
 
+    /**
+     * Calls menus
+     * @param sc
+     * @throws MercatException
+     */
     public void gestioMenu(Scanner sc) throws MercatException {
         Menu menu = new Menu<>("Menu Principal", MENU_OPTIONS.values());
         menu.setDescripcions(mainMenu);
@@ -121,71 +174,73 @@ public class MercatUB {
         CLIENT_OPTIONS cOption = null;
         ORDER_OPTIONS oOption = null;
         do {
-            menu.mostrarMenu();
-            mainOption = (MENU_OPTIONS) menu.getOpcio(sc);
+            try{
+                menu.mostrarMenu();
+                mainOption = (MENU_OPTIONS) menu.getOpcio(sc);
 
-            switch (mainOption){
-                case M_Opcion_1_GestioArticles:
-                    Menu menu1 = new Menu("Menu de Articles", ARTICLE_OPTIONS.values());
-                    menu1.setDescripcions(articleMenu);
-                    menu1.mostrarMenu();
-                    aOption = (ARTICLE_OPTIONS) menu1.getOpcio(sc);
-                    switch (aOption){
-                        case M_Opcion_1_AfegirArticle:
-                            addArticle(sc);
-                            break;
-                        case M_Opcion_2_VisualitzarArticles:
-                            System.out.println(adaptador.recuperaArticles());
-                            break;
-                        case M_Opcion_3_Sortir:
-                            break;
-                    }
-                    break;
-                case M_Opcion_2_GestioClient:
-                    Menu menu2 = new Menu("Menu de Clients", CLIENT_OPTIONS.values());
-                    menu2.setDescripcions(clientMenu);
-                    menu2.mostrarMenu();
-                    cOption = (CLIENT_OPTIONS) menu2.getOpcio(sc);
-                    switch (cOption){
-                        case M_Opcion_1_AfegirClient:
-                            addClient(sc);
-                            break;
-                        case M_Opcion_2_VisualitzarClients:
-                            System.out.println(adaptador.recuperaClients());
-                            break;
-                        case M_Opcion_3_Sortir:
-                            break;
-                    }
-                    break;
-                case M_Opcion_3_GestioComandes:
-                    Menu menu3 = new Menu("Menu de Comandes", ORDER_OPTIONS.values());
-                    menu3.setDescripcions(orderMenu);
-                    menu3.mostrarMenu();
-                    oOption = (ORDER_OPTIONS) menu3.getOpcio(sc);
-                    switch (oOption){
-                        case M_Opcion_1_AfegirComanda:
-                            addOrder(sc);
-                            break;
-                        case M_Opcion_2_EsborrarComanda:
-                            delOrder(sc);
-                            break;
-                        case M_Opcion_3_VisualitzarComanda:
-                            adaptador.recuperaComanda();
-                            break;
-                        case M_Opcion_4_VisualitzarComandaUrgent:
-                            adaptador.recuperaComandaUrgents();
-                            break;
-                        case M_Opcion_5_Sortir:
-                            break;
-                    }
-                    break;
-                case M_Opcion_4_GuardarDades:
+                switch (mainOption){
+                    case M_Opcion_1_GestioArticles:
+                        Menu menu1 = new Menu("Menu de Articles", ARTICLE_OPTIONS.values());
+                        menu1.setDescripcions(articleMenu);
+                        menu1.mostrarMenu();
+                        aOption = (ARTICLE_OPTIONS) menu1.getOpcio(sc);
+                        switch (aOption){
+                            case M_Opcion_1_AfegirArticle:
+                                addArticle(sc);
+                                break;
+                            case M_Opcion_2_VisualitzarArticles:
+                                System.out.println(adaptador.recuperaArticles());
+                                break;
+                            case M_Opcion_3_Sortir:
+                                break;
+                        }
+                        break;
+                    case M_Opcion_2_GestioClient:
+                        Menu menu2 = new Menu("Menu de Clients", CLIENT_OPTIONS.values());
+                        menu2.setDescripcions(clientMenu);
+                        menu2.mostrarMenu();
+                        cOption = (CLIENT_OPTIONS) menu2.getOpcio(sc);
+                        switch (cOption){
+                            case M_Opcion_1_AfegirClient:
+                                addClient(sc);
+                                break;
+                            case M_Opcion_2_VisualitzarClients:
+                                System.out.println(adaptador.recuperaClients());
+                                break;
+                            case M_Opcion_3_Sortir:
+                                break;
+                        }
+                        break;
+                    case M_Opcion_3_GestioComandes:
+                        Menu menu3 = new Menu("Menu de Comandes", ORDER_OPTIONS.values());
+                        menu3.setDescripcions(orderMenu);
+                        menu3.mostrarMenu();
+                        oOption = (ORDER_OPTIONS) menu3.getOpcio(sc);
+                        switch (oOption){
+                            case M_Opcion_1_AfegirComanda:
+                                addOrder(sc);
+                                break;
+                            case M_Opcion_2_EsborrarComanda:
+                                delOrder(sc);
+                                break;
+                            case M_Opcion_3_VisualitzarComanda:
+                                adaptador.recuperaComanda();
+                                break;
+                            case M_Opcion_4_VisualitzarComandaUrgent:
+                                adaptador.recuperaComandaUrgents();
+                                break;
+                            case M_Opcion_5_Sortir:
+                                break;
+                        }
+                        break;
+                    case M_Opcion_4_GuardarDades:
 
-                    break;
-                case M_Opcion_5_CarregaDades:
+                        break;
+                    case M_Opcion_5_CarregaDades:
 
-                    break;
-            }
+                        break;
+                }
+            } catch (MercatException e) { System.out.println(e.getMessage()); }
         }while(mainOption != MENU_OPTIONS.M_Opcion_6_Sortir);
     }
 }
